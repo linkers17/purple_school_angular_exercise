0d09d60560673dd8d76884f53158fb604d9aed50
linkers17 <romeo-gry@mail.ru>
2026-03-08 21:42:53 +0700

    4-router (2)

--- filmly/src/app/shared/guards/auth.guard.ts
+++ filmly/src/app/shared/guards/auth.guard.ts
@@ -1,8 +1,15 @@
-import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
 
 export const authGuard = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   const authService: AuthService = inject(AuthService)
-  return authService.isAuth
+  const router: Router = inject(Router)
+
+  if (!authService.isAuth) {
+    router.navigate(['/public/login'])
+    return false
+  }
+
+  return true
 }
--- filmly/src/app/shared/services/auth.service.ts
+++ filmly/src/app/shared/services/auth.service.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthService {
-  public isAuth = true
+  public isAuth = false
 }
